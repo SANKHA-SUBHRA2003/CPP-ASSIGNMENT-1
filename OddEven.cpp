268dc4ea67499d22d1af22a142f243acbed24bb2
SANKHA-SUBHRA2003 <sg6957@srmist.edu.in>
2023-03-23 21:23:16 +0530

    1st Assignment
    done on 23.03.2023 21:22 pm

---
+++ OddEven.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int n;
+
+  cout << " =====  Program to Check For Odd and Even ===== \n\n";
+  cout << "Enter an integer: ";
+  cin >> n;
+    
+  (n % 2 == 0) ? cout << n << " is even." :  cout << n << " is odd.";
+    
+  return 0;
+}
---
+++ swap.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout << " =====  Program to Swap two numbers without using a 3rd variable ===== \n\n";
+
+    
+    int a,b;
+
+    
+    cout << "Enter the first number : ";
+    cin >> a;
+
+    cout << "Enter the second number : ";
+    cin >> b;
+
+    cout << "\n\nValues Before Swapping:  \n"<<endl;
+    cout << "First Number = " << a <<endl;
+    cout << "Second Number = " << b <<endl;
+
+     
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    cout << "\n\nValues After Swapping:  \n"<<endl;
+
+    cout << "First Number = " << a <<endl;
+    cout << "Second Number = " << b <<endl;
+    cout << "\n\n\n";
+
+    return 0;
+}
